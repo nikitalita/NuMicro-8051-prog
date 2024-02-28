89e40c5a26b7e64c0e4b04875d8866d079837b9e
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-02-28 03:31:48 -0800

    update commands, clarify comments

--- nuvoicp/common/isp_common.h
+++ nuvoicp/common/isp_common.h
@@ -1,35 +1,48 @@
 /**COMMANDS**/
-#define CMD_UPDATE_APROM    0xa0
-#define CMD_UPDATE_CONFIG   0xa1
-#define CMD_READ_CONFIG     0xa2
-#define CMD_ERASE_ALL       0xa3
-#define CMD_SYNC_PACKNO     0xa4
-#define CMD_GET_FWVER       0xa6
-#define CMD_RUN_APROM       0xab
-#define CMD_CONNECT         0xae
-#define CMD_GET_DEVICEID    0xb1
+// Standard commands
+#define CMD_UPDATE_APROM         0xa0
+#define CMD_UPDATE_CONFIG        0xa1
+#define CMD_READ_CONFIG          0xa2
+#define CMD_ERASE_ALL            0xa3
+#define CMD_SYNC_PACKNO          0xa4
+#define CMD_GET_FWVER            0xa6
+#define CMD_RUN_APROM            0xab
+#define CMD_CONNECT              0xae
+#define CMD_GET_DEVICEID         0xb1
+#define CMD_RESET                0xad  // not implemented in default N76E003 ISP rom
+#define CMD_GET_FLASHMODE        0xCA  // not implemented in default N76E003 ISP rom
+#define CMD_RUN_LDROM            0xac  // not implemented in default N76E003 ISP rom
 
-#define CMD_RESET            0xad  // not implemented in default N76E003 ISP rom
-#define CMD_GET_FLASHMODE    0xCA  // not implemented in default N76E003 ISP rom
-#define CMD_RUN_LDROM        0xac   // not implemented in default N76E003 ISP rom
+// Not implemented yet
+#define CMD_RESEND_PACKET        0xFF  // not implemented in default N76E003 ISP rom
 
-#define CMD_READ_ROM        0xa5 // non-official
-#define CMD_GET_UID         0xb2 // non-official
-#define CMD_GET_CID         0xb3 // non-official
-#define CMD_GET_UCID        0xb4 // non-official
-#define CMD_GET_BANDGAP     0xb5 // non-official
-
-
-#define CMD_ISP_PAGE_ERASE      0xD5 // non-official
-
-// probably won't implement these
-#define CMD_WRITE_CHECKSUM   0xC9  // not implemented in default N76E003 ISP rom
-#define CMD_RESEND_PACKET    0xFF  // not implemented in default N76E003 ISP rom
+// Extended commands
+#define CMD_READ_ROM             0xa5 // non-official
+#define CMD_GET_UID              0xb2 // non-official
+#define CMD_GET_CID              0xb3 // non-official
+#define CMD_GET_UCID             0xb4 // non-official
+#define CMD_GET_BANDGAP          0xb5 // non-official
+#define CMD_ISP_PAGE_ERASE       0xD5 // non-official
 
 // Arduino ISP-to-ICP bridge only
-#define CMD_UPDATE_WHOLE_ROM    0xE1  // not implemented in default N76E003 ISP rom
-#define CMD_ISP_MASS_ERASE      0xD6 // non-official
+#define CMD_UPDATE_WHOLE_ROM     0xE1 // non-official
+#define CMD_ISP_MASS_ERASE       0xD6 // non-official
 
+// ** Unsupported by N76E003 **
+// Dataflash commands (when a chip has the ability to deliniate between data and program flash)
+#define CMD_UPDATE_DATAFLASH     0xC3
+// SPI flash commands
+#define CMD_ERASE_SPIFLASH       0xD0
+#define CMD_UPDATE_SPIFLASH      0xD1
+// CAN commands
+#define CAN_CMD_READ_CONFIG      0xA2000000
+#define CAN_CMD_RUN_APROM        0xAB000000
+#define CAN_CMD_GET_DEVICEID     0xB1000000
+
+// Deprecated, no ISP programmer uses these
+#define CMD_READ_CHECKSUM        0xC8
+#define CMD_WRITE_CHECKSUM       0xC9
+#define CMD_SET_INTERFACE        0xBA
 
 // The modes returned by CMD_GET_FLASHMODE
 #define APMODE 1
